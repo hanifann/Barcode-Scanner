8f71a56d8a20e65fcda899a1718062c7816e04fc
hanifan <hanifannurulhaq@outlook.com>
2023-11-04 21:36:57 +0700

    Initial Commit

---
+++ Barcode-Scanner/Barcode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_ScannerApp.swift
+//  Barcode-Scanner
+//
+//  Created by hanifan nurul haq on 04/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Barcode-Scanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Barcode-Scanner
+//
+//  Created by hanifan nurul haq on 04/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
